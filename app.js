7635aeef0cb6742c667e3578839f7dfd345b31b9
Adam Seiwert <adamseiwert@gmail.com>
2016-03-31 21:13:18 -0500

    Added ESLint to the project

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));    
     
 app.get('/', function(req, res) {
-    res.render('index')
+    res.render('index');
 });
     
 app.listen(80, () => {
-  console.log("Listening on port 80...");
+    console.log('Listening on port 80...');
 });
