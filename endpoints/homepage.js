be9b90458039c02116453c1d6e8b6831b36af428
Christian Hughes <ChristianHughes@east-wireless-8-203.reshall.ksu.edu>
2016-05-02 15:47:58 -0500

    Added homepage documentation

--- endpoints/homepage.js
+++ endpoints/homepage.js
@@ -2,10 +2,10 @@
 
 var db = require('../db');
 
-// A controller for the equipment resource
-// This should have methods for all the RESTful actions
+// The Homepage endpoint contains all of the functions pertinent to the Donut Baron Homepage.
 class Homepage {
 
+  // Gets the current donut dollie (ignore variable name), and a list of all upcoming donut dollies. Then renders the homepage.
   getHomepageData(req, res) {
     db.get('SELECT * FROM users WHERE is_donut_baron=?', 1, function(err, donutBaron) {
       if (err) {
@@ -17,28 +17,33 @@ class Homepage {
           console.error(err);
           return res.sendStatus(500);
         };
-        res.render('index', { donutBaron: donutBaron, upcomingUsers: upcomingUsers, user: req.user } );
+        res.render('index', {
+          donutBaron: donutBaron,
+          upcomingUsers: upcomingUsers,
+          user: req.user
+        });
       });
     });
   }
 
+  // Takes care of the donut rating logic, and stores donut ratings in the database.
   RateDonuts(req, res) {
     var reliability = 0;
-    if(typeof req.body.delivered == "undefined") {
-        reliability = 100;
+    if (typeof req.body.delivered == "undefined") {
+      reliability = 100;
     }
     db.run("UPDATE users SET has_rated_this_week=1 WHERE userID=?",
-          req.user.userID);
-    db.get("SELECT * FROM users WHERE is_donut_baron=?", 
-          1, 
+      req.user.userID);
+    db.get("SELECT * FROM users WHERE is_donut_baron=?",
+      1,
       function(err, donutBaron) {
         if (err) {
           console.error(err);
           return res.sendStatus(500);
         }
         db.run("UPDATE users SET donut_quality_rating=?, donut_reliability_rating=?, number_of_ratings=? WHERE userID=?",
-          Math.round(((donutBaron.donut_quality_rating + parseInt(req.body.score))/2) * 10) / 10,
-          Math.floor(donutBaron.donut_reliability_rating + reliability)/(2),
+          Math.round(((donutBaron.donut_quality_rating + parseInt(req.body.score)) / 2) * 10) / 10,
+          Math.floor(donutBaron.donut_reliability_rating + reliability) / (2),
           donutBaron.number_of_ratings + 1,
           donutBaron.userID);
         db.all('SELECT * FROM upcomingList', function(err, upcomingUsers) {
@@ -50,32 +55,34 @@ class Homepage {
         });
       });
   }
-  
-    getComments(req, res) {
-        db.all('SELECT * FROM comments', function(err, commentsArray) {
-            if(err) {
-                console.error(err);
-                return res.sendStatus(500);
-            }
-            res.send(commentsArray);
-        });
-    }
-  
-    addComment(req, res) {
-        db.run('INSERT INTO comments (content, fullname, created, upvote_count, user_has_upvoted) values (?,?,?,?,?)',
-            req.body.comment.content,
-            req.body.currentUser,
-            req.body.comment.created,
-            parseInt(req.body.comment.upvote_count),
-            req.body.comment.user_has_upvoted == 'true' ? 1 : 0
-        );
-        //Need to convert string values back to their original types.
-        req.body.comment.user_has_upvoted = (req.body.comment.user_has_upvoted == 'true' ? true : false);
-        req.body.comment.upvote_count = parseInt(req.body.comment.upvote_count);
-        req.body.comment.created_by_current_user = (req.body.comment.created_by_current_user == 'true' ? true : false);
-        res.send(req.body.comment); 
-    }
-  
+
+  // Gets all of the comments from the database as an array.
+  getComments(req, res) {
+    db.all('SELECT * FROM comments', function(err, commentsArray) {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      res.send(commentsArray);
+    });
+  }
+
+  // Takes care of adding a new comment to the database.
+  addComment(req, res) {
+    db.run('INSERT INTO comments (content, fullname, created, upvote_count, user_has_upvoted) values (?,?,?,?,?)',
+      req.body.comment.content,
+      req.body.currentUser,
+      req.body.comment.created,
+      parseInt(req.body.comment.upvote_count),
+      req.body.comment.user_has_upvoted == 'true' ? 1 : 0
+    );
+    //Need to convert string values back to their original types.
+    req.body.comment.user_has_upvoted = (req.body.comment.user_has_upvoted == 'true' ? true : false);
+    req.body.comment.upvote_count = parseInt(req.body.comment.upvote_count);
+    req.body.comment.created_by_current_user = (req.body.comment.created_by_current_user == 'true' ? true : false);
+    res.send(req.body.comment);
+  }
+
 }
 
 module.exports = exports = new Homepage();
