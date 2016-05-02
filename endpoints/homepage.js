962c8c1277df4ecc5b15ba34e57848fcaeb1146f
apfangman <austinfangman@yahoo.com>
2016-05-02 15:32:47 -0500

    Finally fixed ratings... probably.  Third time's the charm!  Also added comments for the homepage.js

--- endpoints/homepage.js
+++ endpoints/homepage.js
@@ -5,7 +5,7 @@ var db = require('../db');
 // A controller for the equipment resource
 // This should have methods for all the RESTful actions
 class Homepage {
-
+  //Gets and renders the data needed for the homepage
   getHomepageData(req, res) {
     db.get('SELECT * FROM users WHERE is_donut_baron=?', 1, function(err, donutBaron) {
       if (err) {
@@ -21,7 +21,8 @@ class Homepage {
       });
     });
   }
-
+  
+  //Updates the database when a user rates the current donut baron.
   RateDonuts(req, res) {
     var reliability = 0;
     if(typeof req.body.delivered == "undefined") {
@@ -37,8 +38,10 @@ class Homepage {
           return res.sendStatus(500);
         }
         db.run("UPDATE users SET donut_quality_rating=?, donut_reliability_rating=?, number_of_ratings=? WHERE userID=?",
-          Math.round(((donutBaron.donut_quality_rating + parseInt(req.body.score))/2) * 10) / 10,
-          Math.floor(donutBaron.donut_reliability_rating + reliability)/(2),
+          //The current rating is multiplied by the number of ratings, then the new rating is added, then everything is divided by the previous number of ratings + 1. 
+          Math.round((((donutBaron.donut_quality_rating * donutBaron.number_of_ratings) + parseInt(req.body.score))/donutBaron.number_of_ratings + 1) * 10) / 10,
+          //Same as quality, but for reliability.
+          Math.floor((donutBaron.donut_reliability_rating * donutBaron.number_of_ratings) + reliability)/(donutBaron.number_of_ratings + 1),
           donutBaron.number_of_ratings + 1,
           donutBaron.userID);
         db.all('SELECT * FROM upcomingList', function(err, upcomingUsers) {
@@ -50,7 +53,7 @@ class Homepage {
         });
       });
   }
-  
+    //Gets all comments to display them
     getComments(req, res) {
         db.all('SELECT * FROM comments', function(err, commentsArray) {
             if(err) {
@@ -60,7 +63,7 @@ class Homepage {
             res.send(commentsArray);
         });
     }
-  
+    //Adds a new comment into the database when a user makes one.
     addComment(req, res) {
         db.run('INSERT INTO comments (content, fullname, created, upvote_count, user_has_upvoted) values (?,?,?,?,?)',
             req.body.comment.content,
